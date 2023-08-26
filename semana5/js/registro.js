31ba6674003e53c83ddcb2fc79b810cd456af766
Pabs Monestel <pablo.monestel@gmail.com>
2023-08-26 11:58:56 -0600

    update semana 5
    update semana 5

---
+++ semana5/js/registro.js
@@ -0,0 +1,19 @@
+const listaInputs = document.querySelectorAll('input[required]');
+const btnRegistrar = document.querySelector('#btn-registrar');
+const inputNombre = document.querySelector('#fullName');
+
+
+const validar = (event) => {
+    event.preventDefault();
+    listaInputs.forEach(input => {
+        if (input.value == '') {
+            input.classList.add('error');
+            input.classList.remove('correcto');
+        } else {
+            input.classList.remove('error');
+            input.classList.add('correcto');
+        }
+    });
+};
+inputNombre.addEventListener('blur', validar);
+btnRegistrar.addEventListener('click', validar);
