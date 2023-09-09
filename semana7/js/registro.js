d9a8b47a5cffccac545a27582bc9a3b4650c4d32
Pabs Monestel <pablo.monestel@gmail.com>
2023-09-09 09:07:37 -0600

    Semana 7
    Semana 7

---
+++ semana7/js/registro.js
@@ -0,0 +1,63 @@
+const listaInputs = document.querySelectorAll('input[required]');
+const btnRegistrar = document.querySelector('#btn-registrar');
+const inputNombre = document.querySelector('#fullName');
+const inputCorreo = document.querySelector('#email');
+const inputNacimiento = document.getElementById('birthDate');
+const inputContrasenna = document.getElementById('password');
+const inputConfirmacion = document.getElementById('confirmPassword');
+let listaPersonas = [];
+
+const validar = () => {
+
+    let error = false;
+    listaInputs.forEach(input => {
+        if (input.value == '') {
+            error = true;
+            input.classList.add('error');
+            input.classList.remove('correcto');
+        } else {
+            input.classList.remove('error');
+            input.classList.add('correcto');
+        }
+    });
+
+    if (inputContrasenna.value != inputConfirmacion.value) {
+        error = true;
+        inputContrasenna.classList.add('error');
+        inputConfirmacion.classList.add('error');
+
+        inputContrasenna.classList.remove('correcto');
+        inputConfirmacion.classList.remove('correcto');
+    }
+
+    return error;
+};
+
+const registrar = () => {
+    let error = validar();
+    if (error == true) {
+        Swal.fire({
+            'icon': 'warning',
+            'title': 'No se pudo realizar el registro',
+            'text': 'Revise los campos en rojo'
+        });
+    } else {
+        //Variable tipo JSON
+        let personaNueva = {
+            'nombre': inputNombre.value,
+            'correo': inputCorreo.value,
+            'nacimiento': inputNacimiento.value,
+            'contrasenna': inputContrasenna.value
+        };
+        listaPersonas.push(personaNueva);
+
+        Swal.fire({
+            'icon': 'success',
+            'title': 'Usuario registrado correctamente',
+            'text': `Se envío un correo de verificación a la dirección ${personaNueva.correo}`
+        });
+
+    }
+};
+//inputNombre.addEventListener('blur', validar);
+btnRegistrar.addEventListener('click', registrar);
